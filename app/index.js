b3483f5cd3f10179186e7ffa82a6c2af125923c7
Sergey Petrenko <psv9191@mail.ru>
2016-08-29 21:17:36 +0300

    React-learning: routes

--- app/index.js
+++ app/index.js
@@ -1,2 +1,26 @@
-var app = document.getElementById('app');
-app.innerHTML = 'Hello!';
+var React = require('react');
+var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
+var Raven = require('raven-js');
+
+var sentryKey = '14fc0b8567514c7c9826fadbfddec242';
+var sentryApp = '94864';
+var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp;
+
+var _APP_INFO = {
+    name: 'React Learning',
+    branch: 'master',
+    version: '1.0'
+};
+
+Raven.config(sentryURL, {
+    release: _APP_INFO.version,
+    tags: {
+        branch: _APP_INFO.branch
+    }
+}).install();
+
+ReactDOM.render(
+    routes,
+    document.getElementById('app')
+);
